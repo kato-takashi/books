f9509441cf8151c8e883db5debe58046312b915d
Takashi Kato <taka4.kato@gmail.com>
2013-10-23 08:56:29 +0900

    92jpg

--- designPatternJava/FileProperties/src/FileIO.java
+++ designPatternJava/FileProperties/src/FileIO.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 
 public interface FileIO {
-	public void readFromeFile(String filename) throws IOException;
+	public void readFromFile(String filename) throws IOException;
 	public void writeToFile(String filename) throws IOException;
 	public void setValue(String key, String value);
 	public String getValue(String key);
