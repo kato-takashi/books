2e7c0074f033c8a7aa1c82e026d83054e70c25b4
Kato Takashi <taka4.kato@gmail.com>
2013-10-27 10:50:53 +0900

    131jpg

--- 作ればわかるandeoid第二版/HurryUp/gen/com/example/hurryup/R.java
+++ 作ればわかるandeoid第二版/HurryUp/gen/com/example/hurryup/R.java
@@ -21,21 +21,73 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int clear=0x7f020000;
+        public static final int div=0x7f020001;
+        public static final int eq=0x7f020002;
+        public static final int ic_action_search=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int minus=0x7f020005;
+        public static final int mul=0x7f020006;
+        public static final int num1=0x7f020007;
+        public static final int num10=0x7f020008;
+        public static final int num11=0x7f020009;
+        public static final int num12=0x7f02000a;
+        public static final int num2=0x7f02000b;
+        public static final int num3=0x7f02000c;
+        public static final int num4=0x7f02000d;
+        public static final int num5=0x7f02000e;
+        public static final int num6=0x7f02000f;
+        public static final int num7=0x7f020010;
+        public static final int num8=0x7f020011;
+        public static final int num9=0x7f020012;
+        public static final int plus=0x7f020013;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int a_button=0x7f07001a;
+        public static final int b_button=0x7f07001c;
+        public static final int c_button=0x7f07001e;
+        public static final int chronometer=0x7f070003;
+        public static final int eq_button=0x7f07001d;
+        public static final int gridLayout1=0x7f070005;
+        public static final int linearLayout1=0x7f070002;
+        public static final int linearLayout2=0x7f070018;
+        public static final int num_button1=0x7f070006;
+        public static final int num_button10=0x7f07000f;
+        public static final int num_button11=0x7f070010;
+        public static final int num_button12=0x7f070011;
+        public static final int num_button2=0x7f070007;
+        public static final int num_button3=0x7f070008;
+        public static final int num_button4=0x7f070009;
+        public static final int num_button5=0x7f07000a;
+        public static final int num_button6=0x7f07000b;
+        public static final int num_button7=0x7f07000c;
+        public static final int num_button8=0x7f07000d;
+        public static final int num_button9=0x7f07000e;
+        public static final int ope_button1=0x7f070014;
+        public static final int ope_button2=0x7f070015;
+        public static final int ope_button3=0x7f070016;
+        public static final int ope_button4=0x7f070017;
+        public static final int start_button=0x7f070004;
+        public static final int tableLayout1=0x7f070012;
+        public static final int tableRow1=0x7f070013;
+        public static final int tablelayout1=0x7f070019;
+        public static final int text_answer=0x7f070000;
+        public static final int text_result=0x7f070001;
+        public static final int x_button=0x7f07001b;
     }
     public static final class layout {
         public static final int activity_hurry_up=0x7f030000;
     }
-    public static final class menu {
-        public static final int hurry_up=0x7f070000;
-    }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int alert_dialog_ok=0x7f050008;
+        public static final int answer_is=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int chronometer_format=0x7f050003;
+        public static final int correct=0x7f050006;
+        public static final int hantei=0x7f050005;
+        public static final int incorrect=0x7f050007;
+        public static final int result_is=0x7f050002;
+        public static final int start=0x7f050004;
     }
     public static final class style {
         /** 
--- 作ればわかるandeoid第二版/HurryUp/src/com/example/hurryup/HurryUpActivity.java
+++ 作ればわかるandeoid第二版/HurryUp/src/com/example/hurryup/HurryUpActivity.java
@@ -1,22 +1,136 @@
 package com.example.hurryup;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class HurryUpActivity extends Activity {
+	// 数字のImageButton
+	static final int imageButtons[] = { R.id.num_button1, R.id.num_button2,
+			R.id.num_button3, R.id.num_button4, R.id.num_button5,
+			R.id.num_button6, R.id.num_button7, R.id.num_button8,
+			R.id.num_button9, R.id.num_button10, R.id.num_button11,
+			R.id.num_button12, };
+	// 数字の画像
+	static final int numImages[] = { R.drawable.num1, R.drawable.num2,
+			R.drawable.num3, R.drawable.num4, R.drawable.num5, R.drawable.num6,
+			R.drawable.num7, R.drawable.num8, R.drawable.num9,
+			R.drawable.num10, R.drawable.num11, R.drawable.num12, };
+	// 演算子のImageButton
+	static final int opeButtons[] = { R.id.ope_button1, R.id.ope_button2,
+			R.id.ope_button3, R.id.ope_button4, };
+
+	// 演算子の画像
+	static final int opeImages[] = { R.drawable.plus, R.drawable.minus,
+			R.drawable.mul, R.drawable.div, };
+
+	// フィールド
+	private int mAnswer = 0;
+	private int mCount = 0; // 出題済み数　全部で10問
+	private int mRightCount = 0; // 正解数
+	private int mAvalue = 0;
+	private int mBvalue = 0;
+	private int mOpe = 0;
+
+	AnswerNum nums[] = new AnswerNum[imageButtons.length];
+	AnswerNum opes[] = new AnswerOpe[opeButtons.length];
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_hurry_up);
+		createNums();
+		createOpes();
+		setStartButtnListener();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.hurry_up, menu);
-		return true;
+	private void createNums() {
+		for (int i = 0; i < imageButtons.length; ++i) {
+			imageButton imgbtn = (ImageButton) findViewById(imageButtons[i]);
+			nums[i] = new AnswerNum(imgbtn, i);
+		}
+	}
+
+	private void createOpes() {
+		for (int i = 0; i < opeButtons.length; ++i) {
+			imageButton imgbtn = (ImageButton) findViewById(opeButtons[i]);
+			ope[i] = new AnswerNum(imgbtn, i);
+		}
+	}
+
+	private void setStartButtnListener() {
+		// TODO 自動生成されたメソッド
+		Button btn = (Button) findViewById(R.id.start_button);
+		btn.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				startGame();
+			}
+		});
+	}
+
+	private void startGame() {
+		// TODO 自動生成されたメソッド・スタブ
+		mCount = 0;
+		mRightCount = 0;
+		TextView textResult =(TextView)findViewById(R.id.text_result);
+		textResult.setText("");
+		startChronometer();
+		syutudai();
+	}
+	
+	private void syutudai(){
+		//TODO自動生成されたメソッド・スタブ
+		clearAnswer();
+		//出題　（答えを出す）ロジック
+		int a;
+		int b;
+		int swap;
+		int ope;
+		while(true){
+			a = (int)(Math.random()*12+1);
+			b = (int)(Math.random()*12+1);
+			ope = (int)(Math.random()*4+1);
+			if((ope%2) == 0){
+				if(b > a){
+					swap = b;
+					b = a;
+					a = swap;
+				}
+				if(ope == 4){
+					if((a%b) != 0){
+						continue;
+					}
+				}
+			}
+			break;
+		}switch(ope){
+		case 1: //足し算
+			mAnswer = a+b;
+			break;
+		case 2: //引き算
+			mAnswer = a-b;
+			break;
+		case 3: //かけ算
+			mAnswer = a*b;
+			break;
+		case 4: //割り算
+			mAnswer = a/b;
+			break;
+		}
+		Resouces res = getResouces();
+		String ansString = res.getString(R.string.answer_is) + " " + mAnswer;
+		TextView textAnswer = (TextView)findViewById(R.id.text_answer);
+		textAnswer.setText(ansString);
+		
+		private void clearAnswer(){
+			mAvalue = 0;
+			mBvalue = 0;
+			mOpe = 0;
+		}
 	}
 
 }
