83a757ac60d033b799a709ef8d539b8b79352ee4
Kato Takashi <taka4.kato@gmail.com>
2013-11-18 22:00:04 +0900

    249jpg　落とさないで遊ぶゲーム

---
+++ 作ればわかるandeoid第二版/BallCarry/src/com/example/ballcarry/BallCarryActivity.java
@@ -0,0 +1,16 @@
+package com.example.ballcarry;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class BallCarryActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_ball_carry);
+	}
+
+
+}
