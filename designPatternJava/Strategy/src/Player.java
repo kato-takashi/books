09f09181a132d154499045734eafab0c1f809c0a
Kato Takashi <taka4.kato@gmail.com>
2013-12-16 23:38:09 +0900

    182jpg strategy pattern

--- designPatternJava/Strategy/src/Player.java
+++ designPatternJava/Strategy/src/Player.java
@@ -1,5 +1,33 @@
 
 public class Player {
 	//before
+	private String name;
+	private Strategy strategy;
+	private int wincount;
+	private int losecount;
+	private int gamecount;
+	public Player(String name, Strategy strategy){ //名前と戦略を授けられる
+		this.name = name;
+		this.strategy = strategy;
+	}
+	public Hand nexthand(){ //戦略におうかがいをたてる
+		return strategy.nextHand();
+	}
+	public void win(){ //勝った
+		strategy.study(true);
+		wincount++;
+		gamecount++;
+	}
+	public void lose(){ //負けた
+		strategy.study(false);
+		losecount++;
+		gamecount++;
+	}
+	public void even(){ //引き分け
+		gamecount++;
+	}
+	public String toString(){
+		return "[" + ":" + gamecount + "games, " + wincount + "win, " + losecount + "lose" + "]";
+	}
 
 }
