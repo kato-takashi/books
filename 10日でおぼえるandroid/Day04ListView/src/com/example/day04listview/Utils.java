09c3244a75dfe67b863320f7168832171252990c
Takashi Kato <taka4.kato@gmail.com>
2014-01-06 08:52:25 +0900

    189JPG 10days android 公開したデータベースを利用しよう

--- 10日でおぼえるandroid/Day04ListView/src/com/example/day04listview/Utils.java
+++ 10日でおぼえるandroid/Day04ListView/src/com/example/day04listview/Utils.java
@@ -4,103 +4,64 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 
 public class Utils {
+	//コンテンツプロバイダのuri
+	public static final Uri CONTENT_URI = Uri.parse("content://com.example.day04listview/diary");
+	public static final String TABLE_DIARY = "DIARY";
+	public static final String FIELD_DATE = "DATE";
+	public static final String FIELD_CONTENTS =  "CONTENTS";
+
+	//	日付に対応したコンテンツを取得、存在しない場合はNULLを返却
+	//	@param context
+	//	@param date
+	//	@return
+
 	public static String getContents(Context context, long date, String def){
-		DatabaseHelper helper = null;
-		SQLiteDatabase db = null;
 		Cursor cur = null;
-
 		try{
-			//Helperクラスの生成
-			helper = new DatabaseHelper(context);
-			//読み込み用SQLiteDatabaseを生成
-			db = helper.getWritableDatabase();
-
 			//検索処理の実行
-			cur = db.query(DatabaseHelper.TABLE_DIARY, 
-					new String[]{DatabaseHelper.FIELD_CONTENTS}, 
-					DatabaseHelper.FIELD_DATE + " =?", 
-					new String[]{Long.toString(date)}, null, null, null, "1");
-
+			cur = context.getContentResolver().query(CONTENT_URI, new String[]{FIELD_CONTENTS}, FIELD_DATE+"=?", new String[]{Long.toString(date)}, null);
 			if(cur != null && cur.moveToFirst()){
-				String contents = cur.getString(cur.getColumnIndex(DatabaseHelper.FIELD_CONTENTS));
+				String contents = cur.getString(cur.getColumnIndex(FIELD_CONTENTS));
 				return contents;
 			}else{
 				return def;
 			}
-
 		}finally{
 			//終了処理
 			if(cur != null){
 				cur.close();
 			}
-			if(db != null){
-				db.close();
-			}
-
-			if(helper != null){
-				helper.close();
-			}
-
 		}
 	}
 
 	//日付に該当するコンテンツを更新
+	//	@param context
+	//	@param date
+	//	@return
+
 	public static void updateContents(Context context, long date, String contents){
-		DatabaseHelper helper = null;
-		SQLiteDatabase db = null;
+		//書き込み用のデータを作成する
+		ContentValues values = new Contentvalues();
+		values.put(FIELD_CONTENTS, contents);
 		
-		try{
-			//Helperクラスの生成
-			helper = new DatabaseHelper(context);
-			//書き込み用SQLiteDatabaseを生成
-			db = helper.getWritableDatabase();
-			
-			//書き込み用のデータベースを作成
-			ContentValues values = new ContentValues();
-			values.put(DatabaseHelper.FIELD_CONTENTS, contents);
-			
-			//アップデート処理の実行
-			db.update(DatabaseHelper.TABLE_DIARY, values, 
-					DatabaseHelper.FIELD_DATE+" = ?", 
-					new String[]{Long.toString(date)});
-		}finally{
-			//終了処理
-			if(db != null){
-				db.close();
-			}
-			if(helper != null){
-				helper.close();
-			}
-		}
+		//更新処理の実行をする
+		context.getContentResolver().update(CONTENT_URI, values, FIELD_DATE+"=?", new String[]{Long.toString(date)});
 	}
-	
+
 	//コンテンツを新しく登録
+	//	@param context
+	//	@param date
+	//	@return
 	public static void createContents(Context context, long date, String contents){
-		DatabaseHelper helper = null;
-		SQLiteDatabase db = null;
-		try{
-			//Helperクラスの生成
-			helper = new DatabaseHelper(context);
-			//書き込み用SQLitedatabaseを生成
-			db = helper.getWritableDatabase();
-			
-			//書き込み用のデータを作成
-			ContentValues values = new ContentValues();
-			values.put(DatabaseHelper.FIELD_DATE, date);
-			values.put(DatabaseHelper.FIELD_CONTENTS, contents);
-			
-			//インサート処理の実行
-			db.insert(DatabaseHelper.TABLE_DIARY, null, values);
-		}finally{
-			//終了処理
-			if(db != null){
-				db.close();
-			}
-			if(helper != null){
-				helper.close();
-			}
-		}
+		//書き込み用のデータを作成する
+		ContentValues values = new Contentvalues();
+		values.put(FIELD_DATE, date);
+		values.put(FIELD_CONTENTS, contents);
+		
+		//追加処理の実行をする
+		context.getContentResolver().insert(CONTENT_URI, values);
 	}
 }
