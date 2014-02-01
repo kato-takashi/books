011e8237f8e59191fe23452e880d288700b4939d
Kato Takashi <taka4.kato@gmail.com>
2014-02-01 21:44:10 +0900

    Interprenter  修正 designPattern

--- designPatternJava/Interprenter/src/CommandListNode.java
+++ designPatternJava/Interprenter/src/CommandListNode.java
@@ -1,16 +1,24 @@
+import java.util.ArrayList;
+
 //<command> ::= <repeat command> | <primitive command>
 public class CommandListNode extends Node{
+	private ArrayList list = new ArrayList(); 
 	private Node node;
 	public void parse(Context context) throws ParseException {
-		if(context.currentToken().equals("repeat")){
-			node = new RepeatCommandNode();
-			node.parse(context);
-		}else{
-			node = new PrimitiveCommandNode();
-			node.parse(context);
+		while(true){
+			if(context.currentToken()==null){
+				throw new ParseException("Missing 'end'");
+			}else if(context.currentToken().equals("end")){
+				context.skipToken("end");
+				break;
+			}else{
+				Node commandNode = new CommandNode();
+				commandNode.parse(context);
+				list.add(commandNode);
+			}
 		}
 	}
 	public String toString(){
-		return node.toString();
+		return list.toString();
 	}
 }
